0da5a98d752818705bac2a941c08c538d1bbe05b
Anthony TRON <anthony.tron@kpconsulting.fr>
2022-04-14 16:48:18 +0200

    feat(FormikDemo) schema validation with yup

--- src/demo/FormikDemo.jsx
+++ src/demo/FormikDemo.jsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 
 import { Field, Form, Formik } from 'formik';
 import { Autocomplete, Button, Select, TextField } from '@mui/material';
+import { object, string } from 'yup';
 
 const jobs = [
   'Developer',
@@ -10,6 +11,15 @@ const jobs = [
   'Student',
   'Professor',
 ];
+
+const validationSchema = object().shape({
+  firstName: string().required(),
+  lastName: string().required(),
+  email: string().email().required(),
+  job: object().shape({
+    label: string().required()
+  }).required(),
+});
 
 // Thank you! https://github.com/mui/material-ui/issues/18331#issuecomment-569981389
 const FormikAutocomplete = ({form, field, textFieldProps, ...props}) => {
@@ -37,13 +47,16 @@ export default function FormikDemo() {
 
   return (
     <Formik
-      initialValues={{email: '', firstName: '', lastName: '', job: ''}}
+      initialValues={{email: '', firstName: '', lastName: '', job: undefined }}
       onSubmit={handleSubmit}
+      validationSchema={validationSchema}
     >
       <Form>
         <Field as={TextField} type="email" name="email" label="Email"/>
 
         <Field as={TextField} type="text" name="firstName" label="First name"/>
+
+        <Field as={TextField} type="text" name="lastName" label="Last name"/>
 
         <Field
           component={FormikAutocomplete}
