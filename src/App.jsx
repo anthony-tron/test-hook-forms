e9f47f3946ed2874ea65ebe402608824bf84fdb9
Anthony TRON <anthony.tron@kpconsulting.fr>
2022-04-15 11:51:22 +0200

    feat(App) split views using Tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
+import { useState } from 'react';
+import { CssBaseline, Grid, Tab, Tabs } from '@mui/material';
 import FormikDemo from './demo/FormikDemo';
-import { CssBaseline } from '@mui/material';
 import ReactHookFormDemo from './demo/ReactHookFormDemo';
 
 export default function App() {
+
+  const [index, setIndex] = useState(0);
 
   return (
     <>
       <CssBaseline />
 
-      <h1>
-        Formik demo
-      </h1>
-      <FormikDemo />
+      <Grid container gap={3} sx={{ padding : 3 }}>
+        <Grid container item>
+          <Tabs value={index} onChange={(_, newIndex) => setIndex(newIndex)}>
+            <Tab label="Formik" />
+            <Tab label="React Hook Form" />
+          </Tabs>
+        </Grid>
 
-      <h1>
-        ReactHookFormDemo
-      </h1>
-      <ReactHookFormDemo />
-
+        <Grid item>
+          {index === 0 ? (
+            <FormikDemo />
+          ) : (
+            <ReactHookFormDemo />
+          )}
+        </Grid>
+      </Grid>
     </>
   );
 }
